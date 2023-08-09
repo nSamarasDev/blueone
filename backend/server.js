10f130a176ccfd5a1f5565aeb7b60115e7125178
“Nicholas <n.samaras4@outlook.com>
2023-08-09 14:55:48 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
+const path = require('path')
+const morgan = require('morgan')
+const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const colors = require('colors')
 
 const PORT = process.env.PORT || 5050
+
+const app = express()
+
+connectDB()
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
